36565adcca45956b2420ebea1d7488a28984b7fa
Yan Bezborodov <bezborodov30@yandex.ru>
2018-01-18 01:47:04 +0300

    [persistent_set.h]     bezborodov::pointers::shared_ptr:         1) removed 'explicit shared_ptr(T*)' constructor noexcept marker(because we can recieve bad_alloc exception when allocating memory for counter_);         2) added 'size_t count() const noexcept' method to recive count of pointers that points to object;         3) added definition of all shared_ptr methods     bezborodov::pointers::linked_ptr:         1) fixed mistake in linked_ptr constructor('extern' replaced with 'explicit') [main.cpp]     1) added some stupid tests [CMakeLists.txt]     1) added '-DDEBUG' definition for debug build

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
+#include "persistent_set.h"
 
 using namespace std;
+using namespace bezborodov;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+template<typename T, template<typename> class smart_ptr>
+void check() {
+    smart_ptr<T> ptr1; // default ctor
+    smart_ptr<T> ptr2(ptr1); // copy ctor
+    smart_ptr<T> ptr3(std::move(ptr1)); // move ctor
+    ptr3 = ptr2; // copy assign
+    ptr3 = std::move(ptr1); // move assign
+    smart_ptr<T> ptr4(new T);
+    ptr4.reset(new T);
+}
+
+int main() {
+    check<int, pointers::shared_ptr>();
+    check<std::string, pointers::shared_ptr>();
     return 0;
 }
--- persistent_set.h
+++ persistent_set.h
@@ -1,8 +1,9 @@
 #ifndef PERSISTENT_SET_H
 #define PERSISTENT_SET_H
 
-#include <cstddef> // for size_t, nullptr
-#include <utility> // for swap
+#include <cstddef>  // for size_t, nullptr
+#include <utility>  // for swap
+#include <iostream> // for cerr
 
 namespace bezborodov {
 
@@ -20,17 +21,108 @@ public:
     shared_ptr<T>& operator =(shared_ptr<T> &&) noexcept;
     ~shared_ptr() noexcept;
 
-    explicit shared_ptr(T*) noexcept;
+    explicit shared_ptr(T*);
     T* operator ->() const noexcept;
     T& operator*() const noexcept;
 
     T* get() const noexcept;
     void reset(T*) noexcept;
+    size_t count() const noexcept;
     void swap(shared_ptr<T> &) noexcept;
 
     template<typename U>
     friend void swap(shared_ptr<U> &, shared_ptr<U> &) noexcept;
 };
+
+template<typename T>
+constexpr shared_ptr<T>::shared_ptr() noexcept
+    : counter_(nullptr), ptr_(nullptr)
+{}
+
+template<typename T>
+inline shared_ptr<T>::shared_ptr(const shared_ptr<T> & other) noexcept
+    : counter_(other.counter_), ptr_(other.ptr_)
+{
+    if (counter_) {
+        ++*counter_;
+    }
+}
+
+template<typename T>
+inline shared_ptr<T>::shared_ptr(shared_ptr<T> && other) noexcept
+    : shared_ptr()
+{
+    swap(other);
+}
+
+template<typename T>
+inline shared_ptr<T>& shared_ptr<T>::operator =(shared_ptr<T> const & other) noexcept
+{
+    shared_ptr<T> temp(other);
+    swap(temp);
+    return *this;
+}
+
+template<typename T>
+inline shared_ptr<T>& shared_ptr<T>::operator =(shared_ptr<T> && other) noexcept
+{
+    swap(other);
+    return *this;
+}
+
+template<typename T>
+inline shared_ptr<T>::shared_ptr(T * ptr)
+    : counter_(nullptr), ptr_(ptr)
+{
+    if (ptr_) {
+        counter_ = new size_t(1);
+    }
+}
+
+template<typename T>
+inline shared_ptr<T>::~shared_ptr() noexcept {
+    if (counter_ && !--*counter_) {
+        delete ptr_;
+        delete counter_;
+    }
+}
+
+template<typename T>
+inline T* shared_ptr<T>::operator ->() const noexcept {
+    return ptr_;
+}
+
+template<typename T>
+inline T& shared_ptr<T>::operator *() const noexcept {
+    return *ptr_;
+}
+
+template<typename T>
+inline T* shared_ptr<T>::get() const noexcept {
+    return ptr_;
+}
+
+template<typename T>
+inline void shared_ptr<T>::reset(T * ptr) noexcept {
+    shared_ptr<T> temp(ptr);
+    swap(temp);
+}
+
+template<typename T>
+inline size_t shared_ptr<T>::count() const noexcept {
+    return counter_ ? *counter_ : 0;
+}
+
+template<typename T>
+inline void shared_ptr<T>::swap(shared_ptr<T> & other) noexcept {
+    std::swap(counter_, other.counter_);
+    std::swap(ptr_, other.ptr_);
+}
+
+template<typename T>
+inline void swap(shared_ptr<T> & a, shared_ptr<T> & b) noexcept {
+    std::swap(a, b);
+}
 
 template<typename T>
 class linked_ptr
@@ -46,7 +138,7 @@ public:
     linked_ptr<T>& operator =(linked_ptr<T> &&) noexcept;
     ~linked_ptr() noexcept;
 
-    extern linked_ptr(T*) noexcept;
+    explicit linked_ptr(T*) noexcept;
     T* operator ->() const noexcept;
     T& operator*() const noexcept;
 
